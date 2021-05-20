f52833ddee1d45a8eb66263cbfb64417bb76c64e
yqq <youngqqcn@gmail.com>
2021-05-20 17:54:12 +0800

    update rust codes

--- src/0001_two_sum/two_sum.rs
+++ src/0001_two_sum/two_sum.rs
@@ -20,6 +20,17 @@ impl Solution {
         }
         return vec![];
     }
+
+    pub fn two_sum2(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for i  in 0..nums.len() { 
+            for j in i+1..nums.len(){
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
+                }
+            }
+        }
+        vec![]
+    }
 }
 
 
@@ -30,6 +41,15 @@ fn main() {
 
     let ret = Solution::two_sum(vec![1, -1, 5, 7, 8], 4);
     assert_eq!(vec![1, 2], ret);
+
+    let ret = Solution::two_sum(vec![], 0);
+    let empty: Vec<i32> =  Vec::new();
+    assert_eq!(empty, ret);
+
+
+    let ret = Solution::two_sum2(vec![1, 3, 5, 7, 8], 15);
+    assert_eq!(vec![3, 4], ret);
+
 
     println!("test passed");
 }
