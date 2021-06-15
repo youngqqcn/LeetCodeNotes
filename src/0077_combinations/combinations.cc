408276c3ed543e6aefb4f5be4aaf5cdf77b3c4f4
yqq <youngqqcn@163.com>
2021-06-15 20:06:31 +0800

    add combinations codes

--- src/0077_combinations/combinations.cc
+++ src/0077_combinations/combinations.cc
@@ -10,8 +10,6 @@
 #include <memory>
 using namespace std;
 
-
-
 /*
 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
 
@@ -30,29 +28,58 @@ using namespace std;
 
 */
 
-
-class Solution {    
+class Solution
+{
+private:
+    vector<vector<int>> result;
+    vector<int> nums;
 public:
-    vector<vector<int>> combine(int n, int k) {
+    void backtracking(int startIndex, int n, int k)
+    {
+        // 递归终止
+        if (nums.size() == k)
+        {
+            result.push_back(nums);
+            return;
+        }
 
+        // 遍历
+        for (int j = startIndex; j <= n; j++)
+        {
+            nums.push_back(j);
+            backtracking(j + 1, n, k);
+            // 回溯
+            nums.pop_back();
+        }
+    }
+
+    // 回溯法
+    vector<vector<int>> combine(int n, int k)
+    {
+        result.clear();
+        nums.clear();
+
+        backtracking(1, n, k);
+        return result;
     }
 };
-
 
 void test(int n, int k, set<vector<int>> expected)
 {
     Solution sol;
     auto result = sol.combine(n, k);
 
-    if(result.size() != expected.size()) {
+    if (result.size() != expected.size())
+    {
         cout << "FAILED" << endl;
         return;
     }
 
-    for(auto v : result)
+    for (auto v : result)
     {
         auto it = expected.find(v);
-        if( it == expected.end()) {
+        if (it == expected.end())
+        {
             cout << "FAILED" << endl;
         }
         expected.erase(it);
@@ -61,17 +88,15 @@ void test(int n, int k, set<vector<int>> expected)
     cout << "PASSED" << endl;
 }
 
-
 int main()
 {
-//      [2,4],
-//   [3,4],
-//   [2,3],
-//   [1,2],
-//   [1,3],
-//   [1,4],
+    //   [2,4],
+    //   [3,4],
+    //   [2,3],
+    //   [1,2],
+    //   [1,3],
+    //   [1,4],
     test(4, 2, {{2, 4}, {3, 4}, {2, 3}, {1, 2}, {1, 3}, {1, 4}});
     cout << "hello world" << endl;
     return 0;
 }
-
