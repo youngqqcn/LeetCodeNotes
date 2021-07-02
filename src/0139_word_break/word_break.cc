789d6c5ee223f8ea7638c228e772adf88b4794b8
yqq <youngqqcn@gmail.com>
2021-07-02 13:37:41 +0800

    update codes

--- src/0139_word_break/word_break.cc
+++ src/0139_word_break/word_break.cc
@@ -10,8 +10,10 @@
 #include <memory>
 #include <queue>
 #include <stack>
+#include <unordered_set>
+#include <stdlib.h>
+#include <string.h>
 using namespace std;
-
 
 /*
 
@@ -39,18 +41,116 @@ using namespace std;
 
 */
 
-class Solution {
-public:
-    bool wordBreak(string s, vector<string>& wordDict) {
+// 思路:
+//    1. wordDict 中的字符长度必须小于等于 s的字符长度
+//    2. wordDict 中的元素, 是 s 的子串
 
+class Solution
+{
+public:
+    // 跳转遍历
+    bool traverse(vector<vector<bool>> &tb, int row, int col, string &tmpstr, const string &rawstr)
+    {
+        if (row >= tb.size() || col >= tb.size() )
+            return false;
+
+        for (int j = col; j < tb.size(); j++)
+        {
+            if (tb[row][j])
+            {
+                string baktmpstr = tmpstr;
+                string substr = rawstr.substr(row, j - row + 1);
+                tmpstr += substr;
+                if (tmpstr == rawstr)
+                    return true;
+                if( 0 != rawstr.find(tmpstr))
+                    continue;
+                if (traverse(tb, j + 1, j + 1, tmpstr, rawstr))
+                {
+                    return true;
+                }
+                // 回溯
+                tmpstr = baktmpstr;
+            }
+        }
+        return false;
+    }
+
+    // 暴力搜索
+    bool wordBreak_v1(string s, vector<string> &wordDict)
+    {
+        // 首先对字符进行对比
+        set<char> set1(s.begin(), s.end());
+        set<char> set2;
+        for_each(wordDict.begin(), wordDict.end(), [&set2](string tmp) {
+            set2.insert(tmp.begin(), tmp.end());
+        });
+
+        // set1 是 set2的子集
+        for(auto item : set1)
+        {
+            if(set2.find(item) == set2.end())
+                return false;
+        }
+
+
+        unordered_set<string> words(wordDict.begin(), wordDict.end());
+        vector<vector<bool>> tb(s.size(), vector<bool>(s.size(), false));
+        string tmp(s.size(), ' ');
+        bool firstRow = false, lastCol = false;
+        for (int r = 0; r < tb.size(); r++)
+        {
+            vector<int> line;
+            for (int c = r; c < tb[r].size(); c++)
+            {
+                string substr = s.substr(r, c - r + 1);
+                if (words.find(substr) != words.end())
+                {
+                    if(r == 0) firstRow = true;
+                    if(c == tb[r].size() - 1) lastCol = true;
+                    tb[r][c] = true;
+                    line.push_back(c);
+                }
+            }
+        }
+
+        string tmpstr = "";
+        return (firstRow && lastCol) && traverse(tb, 0, 0, tmpstr, s);
+    }
+
+    // https://leetcode-cn.com/problems/word-break/solution/dan-ci-chai-fen-by-leetcode-solution/
+    bool wordBreak(string s, vector<string> &wordDict)
+    {
+        unordered_set<string> words(wordDict.begin(), wordDict.end());
+
+        // dp[i] 表示 s[0..i-1] 字符串是否在字典中
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+
+        string substr = "";
+        for(int i = 1; i < dp.size(); i++)
+        {
+            for(int j = 0; j < i ; j++)
+            {
+                if(dp[j]) {
+                    substr = s.substr(j, i - j);
+                    if( dp[j] && words.find(substr) != words.end()) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+                substr = "";
+            }
+        }
+
+        return dp[dp.size() - 1];
     }
 };
-
 
 void test(string s, vector<string> wordDict, bool expected)
 {
     Solution sol;
-    if(expected == sol.wordBreak(s, wordDict))
+    if (expected != sol.wordBreak(s, wordDict))
     {
         cout << "FAILED" << endl;
         return;
@@ -58,14 +158,17 @@ void test(string s, vector<string> wordDict, bool expected)
     cout << "PASSED" << endl;
 }
 
-
-
 int main()
 {
     test("leetcode", {"leet", "code"}, true);
     test("applepenapple", {"apple", "pen"}, true);
     test("catsandog", {"cats", "dog", "sand", "and", "cat"}, false);
-    cout << "hello world" << endl;
+
+    // "bb" 拆分成 b b  , b 在字典中
+    test("bb", {"a","b","bbb","bbbb"}, true);
+    test("fohhemkkaecojceoaejkkoedkofhmohkcjmkggcmnami", {"kfomka", "hecagbngambii", "anobmnikj", "c", "nnkmfelneemfgcl", "ah", "bgomgohl", "lcbjbg", "ebjfoiddndih", "hjknoamjbfhckb", "eioldlijmmla", "nbekmcnakif", "fgahmihodolmhbi", "gnjfe", "hk", "b", "jbfgm", "ecojceoaejkkoed", "cemodhmbcmgl", "j", "gdcnjj", "kolaijoicbc", "liibjjcini", "lmbenj", "eklingemgdjncaa", "m", "hkh", "fblb", "fk", "nnfkfanaga", "eldjml", "iejn", "gbmjfdooeeko", "jafogijka", "ngnfggojmhclkjd", "bfagnfclg", "imkeobcdidiifbm", "ogeo", "gicjog", "cjnibenelm", "ogoloc", "edciifkaff", "kbeeg", "nebn", "jdd", "aeojhclmdn", "dilbhl", "dkk", "bgmck", "ohgkefkadonafg", "labem", "fheoglj", "gkcanacfjfhogjc", "eglkcddd", "lelelihakeh", "hhjijfiodfi", "enehbibnhfjd", "gkm", "ggj", "ag", "hhhjogk", "lllicdhihn", "goakjjnk", "lhbn", "fhheedadamlnedh", "bin", "cl", "ggjljjjf", "fdcdaobhlhgj", "nijlf", "i", "gaemagobjfc", "dg", "g", "jhlelodgeekj", "hcimohlni", "fdoiohikhacgb", "k", "doiaigclm", "bdfaoncbhfkdbjd", "f", "jaikbciac", "cjgadmfoodmba", "molokllh", "gfkngeebnggo", "lahd", "n", "ehfngoc", "lejfcee", "kofhmoh", "cgda", "de", "kljnicikjeh", "edomdbibhif", "jehdkgmmofihdi", "hifcjkloebel", "gcghgbemjege", "kobhhefbbb", "aaikgaolhllhlm", "akg", "kmmikgkhnn", "dnamfhaf", "mjhj", "ifadcgmgjaa", "acnjehgkflgkd", "bjj", "maihjn", "ojakklhl", "ign", "jhd", "kndkhbebgh", "amljjfeahcdlfdg", "fnboolobch", "gcclgcoaojc", "kfokbbkllmcd", "fec", "dljma", "noa", "cfjie", "fohhemkka", "bfaldajf", "nbk", "kmbnjoalnhki", "ccieabbnlhbjmj", "nmacelialookal", "hdlefnbmgklo", "bfbblofk", "doohocnadd", "klmed", "e", "hkkcmbljlojkghm", "jjiadlgf", "ogadjhambjikce", "bglghjndlk", "gackokkbhj", "oofohdogb", "leiolllnjj", "edekdnibja", "gjhglilocif", "ccfnfjalchc", "gl", "ihee", "cfgccdmecem", "mdmcdgjelhgk", "laboglchdhbk", "ajmiim", "cebhalkngloae", "hgohednmkahdi", "ddiecjnkmgbbei", "ajaengmcdlbk", "kgg", "ndchkjdn", "heklaamafiomea", "ehg", "imelcifnhkae", "hcgadilb", "elndjcodnhcc", "nkjd", "gjnfkogkjeobo", "eolega", "lm", "jddfkfbbbhia", "cddmfeckheeo", "bfnmaalmjdb", "fbcg", "ko", "mojfj", "kk", "bbljjnnikdhg", "l", "calbc", "mkekn", "ejlhdk", "hkebdiebecf", "emhelbbda", "mlba", "ckjmih", "odfacclfl", "lgfjjbgookmnoe", "begnkogf", "gakojeblk", "bfflcmdko", "cfdclljcg", "ho", "fo", "acmi", "oemknmffgcio", "mlkhk", "kfhkndmdojhidg", "ckfcibmnikn", "dgoecamdliaeeoa", "ocealkbbec", "kbmmihb", "ncikad", "hi", "nccjbnldneijc", "hgiccigeehmdl", "dlfmjhmioa", "kmff", "gfhkd", "okiamg", "ekdbamm", "fc", "neg", "cfmo", "ccgahikbbl", "khhoc", "elbg", "cbghbacjbfm", "jkagbmfgemjfg", "ijceidhhajmja", "imibemhdg", "ja", "idkfd", "ndogdkjjkf", "fhic", "ooajkki", "fdnjhh", "ba", "jdlnidngkfffbmi", "jddjfnnjoidcnm", "kghljjikbacd", "idllbbn", "d", "mgkajbnjedeiee", "fbllleanknmoomb", "lom", "kofjmmjm", "mcdlbglonin", "gcnboanh", "fggii", "fdkbmic", "bbiln", "cdjcjhonjgiagkb", "kooenbeoongcle", "cecnlfbaanckdkj", "fejlmog", "fanekdneoaammb", "maojbcegdamn", "bcmanmjdeabdo", "amloj", "adgoej", "jh", "fhf", "cogdljlgek", "o", "joeiajlioggj", "oncal", "lbgg", "elainnbffk", "hbdi", "femcanllndoh", "ke", "hmib", "nagfahhljh", "ibifdlfeechcbal", "knec", "oegfcghlgalcnno", "abiefmjldmln", "mlfglgni", "jkofhjeb", "ifjbneblfldjel", "nahhcimkjhjgb", "cdgkbn", "nnklfbeecgedie", "gmllmjbodhgllc", "hogollongjo", "fmoinacebll", "fkngbganmh", "jgdblmhlmfij", "fkkdjknahamcfb", "aieakdokibj", "hddlcdiailhd", "iajhmg", "jenocgo", "embdib", "dghbmljjogka", "bahcggjgmlf", "fb", "jldkcfom", "mfi", "kdkke", "odhbl", "jin", "kcjmkggcmnami", "kofig", "bid", "ohnohi", "fcbojdgoaoa", "dj", "ifkbmbod", "dhdedohlghk", "nmkeakohicfdjf", "ahbifnnoaldgbj", "egldeibiinoac", "iehfhjjjmil", "bmeimi", "ombngooicknel", "lfdkngobmik", "ifjcjkfnmgjcnmi", "fmf", "aoeaa", "an", "ffgddcjblehhggo", "hijfdcchdilcl", "hacbaamkhblnkk", "najefebghcbkjfl", "hcnnlogjfmmjcma", "njgcogemlnohl", "ihejh", "ej", "ofn", "ggcklj", "omah", "hg", "obk", "giig", "cklna", "lihaiollfnem", "ionlnlhjckf", "cfdlijnmgjoebl", "dloehimen", "acggkacahfhkdne", "iecd", "gn", "odgbnalk", "ahfhcd", "dghlag", "bchfe", "dldblmnbifnmlo", "cffhbijal", "dbddifnojfibha", "mhh", "cjjol", "fed", "bhcnf", "ciiibbedklnnk", "ikniooicmm", "ejf", "ammeennkcdgbjco", "jmhmd", "cek", "bjbhcmda", "kfjmhbf", "chjmmnea", "ifccifn", "naedmco", "iohchafbega", "kjejfhbco", "anlhhhhg"}, true);
+    test("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", {"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"}, false);
+    test("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", {"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"}, false);
+    test("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", {"aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa","ba"}, false);
     return 0;
 }
-
