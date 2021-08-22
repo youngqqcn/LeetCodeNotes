c184bef7ada21049970709865e9ce6e6dc98209f
yqq <youngqqcn@163.com>
2021-08-22 10:03:38 +0800

    update 0494.目标和

--- src/0494_target_sum/target_sum.cc
+++ src/0494_target_sum/target_sum.cc
@@ -75,15 +75,36 @@ private:
     // 2. 动态规划  TODO
     int finTargetV2(vector<int>& nums, int target)
     {
-        // dp[i] 
+        // dp[i]
         return 0;
     }
 public:
-    int findTargetSumWays(vector<int>& nums, int target)
+    int findTargetSumWays_v1(vector<int>& nums, int target)
     {
         result = 0;
         findTargetV1(nums, target, 0, 0);
         return result;
+    }
+
+
+    int findTargetSumWays(vector<int>& nums, int S)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) sum += nums[i];
+        if (S > sum) return 0; // 此时没有方案
+        if ((S + sum) % 2 == 1) return 0; // 此时没有方案
+        if (S + sum < 0) return 0;
+
+        int bagSize = (S + sum) / 2;
+
+        vector<int> dp(bagSize + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = bagSize; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[bagSize];
     }
 };
 
